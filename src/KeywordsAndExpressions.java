26afa2ed03181491382d71aeac2993a3d0ffa16d
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-02-05 05:55:31 +0530

    Initial Java setup

--- src/KeywordsAndExpressions.java
+++ src/KeywordsAndExpressions.java
@@ -1,2 +1,20 @@
 public class KeywordsAndExpressions {
+
+    public static void main(String[] args) {
+
+    int highscore = 50;
+
+    if (highscore > 25){
+        highscore = highscore + 1000;
+    }
+
+    int health = 100;
+
+    if ((health < 25) && (highscore > 1000)) {
+        highscore = highscore - 1000;
+    }
+
+    //how many expressions are the in the above part
+    // ANSWER is 12
+    }
 }
