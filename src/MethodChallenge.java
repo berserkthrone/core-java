6c1ae46e6362fec4870ec772d1ecf44530c0173d
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-02-06 07:12:42 +0530

    Initial Java setup

--- src/MethodChallenge.java
+++ src/MethodChallenge.java
@@ -4,6 +4,18 @@ public class MethodChallenge {
 
         int highScorePosition = calculateHighScorePosition(1500);
         displayHighScorePosition("JOHN", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Bob", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Percy", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Gilbert", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("James", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
@@ -12,15 +24,16 @@ public class MethodChallenge {
     }
 
     public static int calculateHighScorePosition(int playerScore){
-
+        int position = 0;
         if(playerScore >= 1000){
-            return 1;
-        }else if(playerScore >= 500 && playerScore < 1000){
-            return 2;
-        }else if(playerScore >= 100 && playerScore < 500){
-            return 3;
-        }else{
-            return 4;
+            position = 1;
+        }else if(playerScore >= 500){
+            position = 2;
+        }else if(playerScore >= 100){
+            position = 3;
+        }else {
+            position = 4;
         }
+        return position;
     }
 }
