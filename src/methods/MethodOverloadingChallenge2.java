d67d72ee0b50c04893131e0d0a9e34f083372e81
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-02-08 13:45:33 +0530

    Initial Java setup

--- src/methods/MethodOverloadingChallenge2.java
+++ src/methods/MethodOverloadingChallenge2.java
@@ -3,25 +3,37 @@ package methods;
 public class MethodOverloadingChallenge2 {
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
         System.out.println(getDurationString(65, 45));
-    }
-    public static String getDurationString(int seconds){
-    int minutes = seconds/60;
-    int hours = minutes/60;
+        System.out.println(getDurationString(3945));
 
-    int remainingMinutes = minutes % 60;
-
-    int remainingSeconds = seconds % 60;
-
-    return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
     }
 
-    public static String getDurationString(int minutes, int seconds){
-        int hours = minutes/60;
+    public static String getDurationString(int seconds) {
 
+        if (seconds < 0) {
+            return "Invalid data for seconds(" + seconds + "), must be a positive integer";
+        }
+            int minutes = seconds / 60;
+            return getDurationString(seconds / 60, seconds % 60);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+
+        if (minutes <= 0) {
+            return "Invalid data for minutes(" + minutes +
+                    "), must be a positive minutes";
+        }
+
+        if(seconds < 0 || seconds > 59){
+            return "Invalid data for seconds(" + seconds +
+                    "), must be between 0 and 59";
+        }
+
+        int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
-
         int remainingSeconds = seconds % 60;
 
         return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
