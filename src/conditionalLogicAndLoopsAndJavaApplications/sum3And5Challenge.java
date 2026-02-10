0a5b78bf79a6f2715e80de8c661d507880767219
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-02-10 05:45:39 +0530

    Initial Java setup

---
+++ src/conditionalLogicAndLoopsAndJavaApplications/sum3And5Challenge.java
@@ -0,0 +1,26 @@
+package conditionalLogicAndLoopsAndJavaApplications;
+
+public class sum3And5Challenge {
+
+    public static void main(String[] args) {
+
+        int countOfMatches = 0;
+        int sumOfMatches = 0;
+
+        for (int loopNumber = 1; loopNumber <= 1000; loopNumber++){
+            if((loopNumber % 3 == 0) && (loopNumber % 5 == 0)){
+
+                countOfMatches++;
+                sumOfMatches++;
+                sumOfMatches += loopNumber;
+                System.out.println("Found a match = " + loopNumber);
+            }
+
+            if(countOfMatches == 5){
+                break;
+            }
+        }
+
+        System.out.println("sum = " + sumOfMatches);
+    }
+}
