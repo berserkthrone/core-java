69b870c519d34c99a065fb650aaefdae93d40013
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-03-07 00:23:02 +0530

    Initial Java setup

--- src/OOPsClassAndInheritance/Car.java
+++ src/OOPsClassAndInheritance/Car.java
@@ -1,10 +1,5 @@
 package OOPsClassAndInheritance;
 
-//Public:- public means any other class in any package can access this class.
-//Protected:- protected allow classes in the same package and any sub classes in other packages to have access to the member
-//when the modifier is omitted this has special meaning called package access
-//meaning the member is accessible only to classes in the same package.
-//Private:- private means that no other class can access this member
 
 public class Car {
 
@@ -13,24 +8,6 @@ public class Car {
     private String color = "Gray";
     private int doors = 2;
     private boolean convertible = true;
-
-    // unlike local variables the above ones are class variables
-    // need access modifiers
-
-    //so we are using private classes variables which be access easily
-    //although we can use public classes but it aint a good practise
-    //so we need getter and setters
-    //GETTER :- is a method on a class that retrieves the value of a
-    // private field and returns it.
-    //SETTER :- is a method on a class that sets the value of a private field
-    // the purpose of these methods is to control and protect access to private fields
-
-
-    // This provides encapsulation of the internals of our class and supports
-    //maintenance of a public interface that does not have to change even though our class might.
-
-
-
 
     public String getBrand() {
         return brand;
@@ -85,7 +62,7 @@ public class Car {
 
     //This :- ia a special keyword in java. What it really refers to is the instance
     //that was created when the object was instantiated.
-    //So, "THIS" is a special reference name for the object or instance, ehich it can use
+    //So, "THIS" is a special reference name for the object or instance, which it can use
     // to describe itself.
     //And we can use "THIS" to access fields on the class.
 
