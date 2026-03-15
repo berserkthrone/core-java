9e9f08b72c1d1c3b21944e2f5bdc4d80d1099c86
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-03-16 05:23:35 +0530

    Initial Java setup

--- src/OOPsClassAndInheritance/TheObjectClass/Main.java
+++ src/OOPsClassAndInheritance/TheObjectClass/Main.java
@@ -6,6 +6,9 @@ public class Main extends Object {
 
         Student max = new Student("Max", 21);
         System.out.println(max);
+
+    PrimarySchoolStudent jimmy = new PrimarySchoolStudent("jimmy", 8, "carol");
+    System.out.println(jimmy);
     }
 }
 
@@ -32,5 +35,18 @@ class Student {
 //                "name='" + name + '\'' +
 //                ", age=" + age +
 //                '}';
+    }
+}
+
+class PrimarySchoolStudent extends Student{
+    private String parentName;
+    public PrimarySchoolStudent(String name, int age, String parentName) {
+        super(name, age);
+        this.parentName = parentName;
+    }
+
+    @Override
+    public String toString() {
+        return parentName + "'s kid, " + super.toString();
     }
 }
