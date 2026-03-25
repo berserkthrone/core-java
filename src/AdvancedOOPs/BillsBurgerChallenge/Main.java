69b63e6e335e0bb8656d83127a04c133b8d28cdb
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-03-25 05:50:09 +0530

    Initial Java setup

--- src/AdvancedOOPs/BillsBurgerChallenge/Main.java
+++ src/AdvancedOOPs/BillsBurgerChallenge/Main.java
@@ -16,15 +16,22 @@ public class Main {
 //        burger.addToppings("BACON","CHEESE","MAYO");
 //        burger.printItem();
 
-        MealOrder regularMeal = new MealOrder();
-        regularMeal.addBurgerToppings("BACON","CHEESE","MAYO");
-        regularMeal.setDrinkSize("LARGE");
-        regularMeal.printItemizedList();
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("BACON","CHEESE","MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.printItemizedList();
+//
+//        MealOrder secondMeal = new MealOrder("turkey","7-up",
+//                "chili");
+//        secondMeal.addBurgerToppings("LETTUCE","CHEESE","MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
 
-        MealOrder secondMeal = new MealOrder("turkey","7-up",
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up",
                 "chili");
-        secondMeal.addBurgerToppings("LETTUCE","CHEESE","MAYO");
-        secondMeal.setDrinkSize("SMALL");
-        secondMeal.printItemizedList();
+        deluxeMeal.addBurgerToppings("AVOCADO","BACON","LETTUCE",
+                "CHEESE","MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
     }
 }
