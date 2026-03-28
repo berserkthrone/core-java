17369c7d99fcb1c909f84273818bd5b00b6fb898
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-03-28 06:45:36 +0530

    Initial Java setup

--- src/ArrayLists/DescendingChallenge/Main.java
+++ src/ArrayLists/DescendingChallenge/Main.java
@@ -10,8 +10,8 @@ public class Main {
         int[] firstArray = getRandomArray(10);
         System.out.println(Arrays.toString(firstArray));
 
-        Arrays.sort(firstArray);
-        System.out.println(Arrays.toString(firstArray));
+        int[] sortedArray = sortIntegers(new int[] {7, 30, 35});
+        System.out.println(Arrays.toString(sortedArray));
 
 
     }
@@ -26,5 +26,24 @@ public class Main {
         return array;
     }
 
-
+    private static int[] sortIntegers(int[] array) {
+        System.out.println(Arrays.toString(array));
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        boolean flag = true;
+        int temp;
+        while(flag){
+            flag = false;
+            for(int i = 0; i < sortedArray.length - 1;i++){
+                if(sortedArray[i] < sortedArray[i + 1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                    System.out.println("---->" + Arrays.toString(sortedArray));
+                }
+            }
+            System.out.println(Arrays.toString(sortedArray));
+        }
+        return sortedArray;
+    }
 }
