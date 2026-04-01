21f041f20a18531b030027a183d6c50a63d076fa
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-04-02 04:53:08 +0530

    Initial Java setup

---
+++ src/Array/ArrayLists/MoreList.java
@@ -0,0 +1,26 @@
+package Array.ArrayLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoreList {
+
+    public static void main(String[] args) {
+
+        String[] items = {"apples", "bananas", "milk", "eggs"};
+
+        List<String> list = List.of(items);
+        System.out.println(list);
+
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add(0,"mango");
+        System.out.println(groceries);
+
+        ArrayList<String> nextList = new ArrayList<>(
+                List.of("pickles", "mustard", "cheese"));
+        System.out.println(nextList);
+
+        groceries.addAll(nextList);
+        System.out.println(groceries);
+    }
+}
