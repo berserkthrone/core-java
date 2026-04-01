e766f3e0d70e2a8ecdde298c07c6c9864379bb3d
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-04-02 04:43:49 +0530

    Initial Java setup

--- src/Array/ArrayLists/Main.java
+++ src/Array/ArrayLists/Main.java
@@ -1,4 +1,38 @@
 package Array.ArrayLists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem (String name, String type, int count) {
+
+        public GroceryItem(String name) {
+            this(name, "DAIRY", 1);
+        }
+
+        @Override
+    public String toString() {
+            return String.format("%d %s in %s", count, name.toUpperCase(), type);
+        }
+    }
 public class Main {
+
+        public static void main(String[] args) {
+            GroceryItem[] groceryArray = new GroceryItem[3];
+            groceryArray[0] = new GroceryItem("milk");
+            groceryArray[1] = new GroceryItem("apples",
+                    "PRODUCE", 6);
+            groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+            System.out.println(Arrays.toString(groceryArray));
+
+            ArrayList objectList = new ArrayList();
+            objectList.add(new GroceryItem("Butter"));
+            objectList.add("Yogurt");
+
+            ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>();
+            groceryList.add(new GroceryItem("Butter"));
+            groceryList.add(new GroceryItem("Bread", "PRODUCE", 4));
+            groceryList.add(0,
+                    new GroceryItem("apples", "PRODUCE", 3));
+            System.out.println(groceryList);
+        }
 }
