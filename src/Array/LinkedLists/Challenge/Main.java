5e94d700e1209af66006fd3d52dfa9632f10fffb
Shivansh Vatsal <shivanshvatsal@gmail.com>
2026-04-03 08:25:24 +0530

    Initial Java setup

--- src/Array/LinkedLists/Challenge/Main.java
+++ src/Array/LinkedLists/Challenge/Main.java
@@ -11,8 +11,6 @@ record Place(String name,int distance){
     }
 }
 
-
-
 public class Main {
 
     public static void main(String[] args) {
@@ -31,50 +29,104 @@ public class Main {
         placesToVisit.addFirst(new Place("Sydney", 0));
         System.out.println(placesToVisit);
 
-
         var iterator = placesToVisit.listIterator();
-        Scanner scanner  = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         boolean quitLoop = false;
         boolean forward = true;
 
         printMenu();
 
-        while(!quitLoop){
+        while (!quitLoop) {
+            if (!iterator.hasPrevious()) {
+                System.out.println("Originating : " + iterator.next());
+                forward = true;
+            }
+            if (!iterator.hasNext()) {
+                System.out.println("Final : " + iterator.previous());
+                forward = false;
+            }
             System.out.print("Enter Value: ");
-            String menuItem = scanner.nextLine().toLowerCase().substring(0,1);
+            String menuItem = scanner.nextLine().toUpperCase().substring(0, 1);
+
+            switch (menuItem) {
+                case "F":
+                    System.out.println("User wants to go forward");
+                    if (!forward) {           // Reversing Direction
+                        forward = true;
+                        if (iterator.hasNext()) {
+                            iterator.next();  // Adjust position forward
+                        }
+                    }
+
+                    if (iterator.hasNext()) {
+                        System.out.println(iterator.next());
+                    }
+
+                    break;
+
+                case "B":
+                    System.out.println("User wants to go backwards");
+                    if (forward) {           // Reversing Direction
+                        forward = false;
+                        if (iterator.hasPrevious()) {
+                            iterator.previous();  // Adjust position backwards
+                        }
+                    }
+
+                    if (iterator.hasPrevious()) {
+                        System.out.println(iterator.previous());
+                    }
+                    break;
+
+                case "M":
+                    printMenu();
+                    break;
+
+                case "L":
+                    System.out.println(placesToVisit);
+                    break;
+
+                default:
+                    quitLoop = true;
+                    break;
+            }
         }
+
     }
 
-    private static void addPlace(LinkedList<Place> list, Place place){
+    private static void addPlace(LinkedList<Place> list, Place place) {
 
-        if(list.contains(place)){
+        if (list.contains(place)) {
             System.out.println("Found duplicate: " + place);
             return;
         }
 
-        for(Place p:list){
-            if(p.name().equalsIgnoreCase(place.name())){
+        for (Place p : list) {
+            if (p.name().equalsIgnoreCase(place.name())) {
                 System.out.println("Found duplicate: " + place);
                 return;
             }
         }
 
         int matchedIndex = 0;
-        for(var listPlace : list){
-            if(place.distance() <listPlace.distance()){
-                list.add(matchedIndex,place);
+        for (var listPlace : list) {
+            if (place.distance() < listPlace.distance()) {
+                list.add(matchedIndex, place);
                 return;
             }
+
             matchedIndex++;
         }
+
         list.add(place);
     }
 
-    private static void printMenu(){
+    private static void printMenu() {
+
         System.out.println("""
-                Available actions(select word or letter):
+                Available actions (select word or letter):
                 (F)orward
-                (B)ackward
+                (B)ackwards
                 (L)ist Places
                 (M)enu
                 (Q)uit""");
